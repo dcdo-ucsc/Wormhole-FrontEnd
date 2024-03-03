10935f253b0e28f4e746f63bc048979d8a515ffa
Kevin Guan <skaarfet@gmail.com>
2024-03-03 02:15:45 -0800

    Add userApi, fileApi, and sessionApi modules

---
+++ src/api/fileApi.js
@@ -0,0 +1,40 @@
+/* API Docs
+
+https://github.com/dcdo-ucsc/Wormhole-Backend/blob/master/API_DOCS.md
+*/
+import axios from "axios";
+import FormData from "form-data";
+
+const backend = "http://localhost:9001";
+
+const uploadFile = async (files, sessionToken, fileCount) => {
+  let formData = new FormData();
+  files.forEach((file) => {
+    formData.append("files", file);
+  });
+
+  const res = await axios.post(
+    backend + `/api/session/upload/${fileCount}`,
+    formData,
+    {
+      withCredentials: true,
+      headers: {
+        "Content-Type": "multipart/form-data",
+        Authorization: `Bearer ${sessionToken}`,
+      },
+    }
+  );
+  return res.data;
+};
+
+const downloadFile = async (sessionToken) => {
+  const res = await axios.get(backend + `/api/session/download`, {
+    withCredentials: true,
+    headers: {
+      Authorization: `Bearer ${sessionToken}`,
+    },
+  });
+  return res.data;
+};
+
+export { uploadFile, downloadFile };
